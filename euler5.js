a36c4d6272dfb9289cb12d9b0624de34fd136dd3
osama <okpro333@gmail.com>
2022-05-24 14:59:58 -0400

    intro to euler 5

---
+++ euler5.js
@@ -0,0 +1,23 @@
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
+// check 2520 in code first to make sure I get the correct answer
+
+
+
+/*
+
+
+let x be the smallest positive number
+
+the following needs to be true for value x
+
+a = x / 2
+b = x / 3
+c = x / 4
+
+
+*/
