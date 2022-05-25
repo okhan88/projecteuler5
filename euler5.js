b514df905b2e4ceecffbbe6091e1f4b36147a5a8
osama <okpro333@gmail.com>
2022-05-25 17:36:19 -0400

    basic setup v1

--- euler5.js
+++ euler5.js
@@ -7,10 +7,42 @@ What is the smallest positive number that is evenly divisible by all of the numb
 // check 2520 in code first to make sure I get the correct answer
 
 
+function smallestPositNumb(z) {
+
+    for (let x = 0; x <= 200000; x++)
+
+        let a = x / 2
+        let b = x / 3
+        let c = x / 4
+        let d = x / 5
+        let e = x / 6
+        let f = x / 7
+        let g = x / 8
+        let h = x / 9
+        let i = x / 10
+        let j = x / 11
+        let k = x / 12
+        let l = x / 13
+        let m = x / 14
+        let n = x / 15
+        let o = x / 16 
+        let p = x / 17
+        let q = x / 18
+        let r = x / 19
+        let s = x / 20
+
+        if (Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c) && Number.isInteger(d) && Number.isInteger(e) && Number.isInteger(f) && Number.isInteger(g) && Number.isInteger(h) && Number.isInteger(i) && Number.isInteger(j) && Number.isInteger(k) && Number.isInteger(l) && Number.isInteger(m) && Number.isInteger(n) && Number.isInteger(o) && Number.isInteger(p) && Number.isInteger(q) && Number.isInteger(r) && Number.isInteger(s))
+
+
+
+
+}
+
+
+
+
 
 /*
-
-
 let x be the smallest positive number
 
 the following needs to be true for value x
@@ -18,6 +50,23 @@ the following needs to be true for value x
 a = x / 2
 b = x / 3
 c = x / 4
+d = x / 5
+e = x / 6
+f = x / 7
+g = x / 8
+h = x / 9
+i = x / 10
+j = x / 11
+k = x / 12
+l = x / 13
+m = x / 14
+n = x / 15
+o = x / 16 
+p = x / 17
+q = x / 18
+r = x / 19
+s = x / 20
+
 
 
 */
